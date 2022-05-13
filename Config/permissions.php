7da4c949237a7f0c92d815538f1a25ee6d3bc7aa
William Verde <williamverde10@gmail.com>
2022-05-13 17:35:12 -0400

    Added: permision edit created by

--- Config/permissions.php
+++ Config/permissions.php
@@ -8,8 +8,9 @@ return [
         'create' => 'requestable::requestables.create resource',
         'edit' => 'requestable::requestables.edit resource',
         'destroy' => 'requestable::requestables.destroy resource',
-      'edit-status' => 'requestable::requestables.edit-status resource',
-      'edit-eta-date' => 'requestable::requestables.edit-eta-date resource',
+        'edit-status' => 'requestable::requestables.edit-status resource',
+        'edit-eta-date' => 'requestable::requestables.edit-eta-date resource',
+        'edit-created-by' => 'requestable::requestables.edit-created-by resource',
     ],
 
     'requestable.categories' => [
