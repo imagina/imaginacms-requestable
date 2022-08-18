73405708fe61bbcfceaa9bd186802ea2ee8fc964
William Verde <williamverde10@gmail.com>
2022-08-18 09:46:53 -0400

    Update: Requestable seeder change process to method create config

--- Database/Seeders/RequestableTableSeeder.php
+++ Database/Seeders/RequestableTableSeeder.php
@@ -30,60 +30,13 @@ class RequestableTableSeeder extends Seeder
       "fields" => [],
     ];
     $requestableConfig = $requestableRepository->moduleConfigs();
+
+    $requestableService = app("Modules\Requestable\Services\RequestableService");
     
     foreach ($requestableConfig as $config) {
-      $params = [
-        "filter" => [
-          "field" => "type",
-        ],
-        "include" => [],
-        "fields" => [],
-      ];
-      
-      if (isset($config["type"])) {
-        
-        $category = $categoryRepository->getItem($config["type"] ?? '', json_decode(json_encode($params)));
-        
-        if (!isset($category->id)) {
-          $category = $categoryRepository->create([
-            'type' => $config["type"],
-            'time_elapsed_to_cancel' => $config["timeElapsedToCancel"] ?? -1,
-            'events' => $config["events"] ?? null,
-            'internal' => $config["internal"] ?? 1,
-            'requestable_type' => $config["requestableType"],
-            $locale => [
-              "title" => trans($config["title"])
-            ]
-          ]);
-          
-          if(isset($config["formId"]) && !empty($config["formId"]))
-            event(new SyncFormeable($category, ["form_id" => is_int($config["formId"]) ? $config["formId"] : setting($config["formId"], null, null)]));
-          
-          if (isset($config["useDefaultStatuses"]) && $config["useDefaultStatuses"]) {
-            $statuses = (new DefaultStatus())->lists();
-          } else {
-            $statuses = $config["statuses"];
-          }
-          
-          foreach ($statuses as $key => $status) {
-       
-            $statusRepository->create([
-                "category_id" => $category->id,
-                'value' => $key,
-                'final' => $status["final"] ?? false,
-                'default' => $config["defaultStatus"] ?? $status["default"] ?? false,
-                'cancelled_elapsed_time' => $config["statusToSetWhenElapsedTime"] ?? $status["cancelled_elapsed_time"] ?? false,
-                'events' => $config["eventsWhenStatus"][$key] ?? $status["events"] ?? null,
-                'delete_request' => $config["deleteWhenStatus"][$key] ??  $status["delete_request"] ?? false,
-                $locale => [
-                  "title" => trans($status["title"])
-                ]
-              ]
-            );
-          }
-        }
-      }
-      
+      $requestableService->createFromConfig($config);
     }
+
   }
+
 }
