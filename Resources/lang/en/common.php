2b2a4560841c8d23e1cca859bf1428720f32d05f
William Verde <williamverde10@gmail.com>
2022-09-08 15:08:56 -0400

    Added: translations in common

--- Resources/lang/en/common.php
+++ Resources/lang/en/common.php
@@ -8,6 +8,10 @@ return [
     'inProgress' => 'In Progress',
     'completed' => 'Completed',
     'cancelled' => 'Cancelled',
-  ]
+  ],
+  'messages' => [
+    'field required' => 'This field is required',
+    'min 2 characters' => 'Minimum of 2 characters',
+  ],
 
 ];
--- Resources/lang/es/common.php
+++ Resources/lang/es/common.php
@@ -8,6 +8,10 @@ return [
     'inProgress' => 'En progreso',
     'completed' => 'Completado',
     'cancelled' => 'Cancelado',
-  ]
+  ],
+  'messages' => [
+    'field required' => 'El campo es requerido',
+    'min 2 characters' => 'El campo debe contener mínimo dos caracteres',
+  ],
 
 ];
