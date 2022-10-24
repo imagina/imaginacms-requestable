fac6b7e9265ecbb4cc8327137971afc8705f0e1f
William Verde <williamverde10@gmail.com>
2022-10-24 09:52:57 -0400

    Added: validation when create an status, new rule

--- Http/Requests/CreateStatusRequest.php
+++ Http/Requests/CreateStatusRequest.php
@@ -3,12 +3,19 @@
 namespace Modules\Requestable\Http\Requests;
 
 use Modules\Core\Internationalisation\BaseFormRequest;
+use Illuminate\Validation\Rule;
+use Modules\Requestable\Rules\CategoryInternal;
 
 class CreateStatusRequest extends BaseFormRequest
 {
     public function rules()
-    {
-        return [];
+    {   
+        $categoryService = app("Modules\Requestable\Services\CategoryService");
+
+        return [
+            'category_id' => "required",
+            'value' => Rule::requiredIf($categoryService->isInternal($this->category_id))
+        ];
     }
 
     public function translationRules()
@@ -23,11 +30,19 @@ class CreateStatusRequest extends BaseFormRequest
 
     public function messages()
     {
-        return [];
+        return [
+            'category_id.required' => trans('requestable::common.messages.field required'),
+        ];
     }
 
     public function translationMessages()
     {
         return [];
     }
+
+    public function getValidator(){
+        return $this->getValidatorInstance();
+    }
+
+
 }
