4ce64df0787d768a280bf155e7aa74bf81ca7011
William Verde <williamverde10@gmail.com>
2022-11-10 11:41:39 -0400

    Added: automation rule api

---
+++ Http/Controllers/Api/AutomationRuleApiController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Modules\Requestable\Http\Controllers\Api;
+
+use Modules\Core\Icrud\Controllers\BaseCrudController;
+//Model
+use Modules\Requestable\Entities\AutomationRule;
+use Modules\Requestable\Repositories\AutomationRuleRepository;
+
+class AutomationRuleApiController extends BaseCrudController
+{
+  public $model;
+  public $modelRepository;
+
+  public function __construct(AutomationRule $model, AutomationRuleRepository $modelRepository)
+  {
+    $this->model = $model;
+    $this->modelRepository = $modelRepository;
+  }
+}
