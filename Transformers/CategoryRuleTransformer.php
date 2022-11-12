e22f60f477ac4be9fa230436e14a087846014db5
msolano <msolano@imaginacolombia.com>
2022-11-11 19:03:56 -0500

    +Add: Translate the label of the formFields in the CategoryRule transformer

--- Transformers/CategoryRuleTransformer.php
+++ Transformers/CategoryRuleTransformer.php
@@ -7,16 +7,27 @@ use Modules\Core\Icrud\Transformers\CrudResource;
 class CategoryRuleTransformer extends CrudResource
 {
   /**
-  * Method to merge values with response
-  *
-  * @return array
-  */
+   * Method to merge values with response
+   *
+   * @return array
+   */
   public function modelAttributes($request)
   {
-    
-    return [
+    $data = [
       'statusName' => $this->statusName,
+      'formFields' => $this->form_fields,
     ];
 
+    //Translate the labels of the formfields
+    if (!is_null($this->form_fields)) {
+      foreach ($this->form_fields as $name => $field){
+        if(isset($data["formFields"]->$name->props->label)){
+          $data["formFields"]->$name->props->label = trans($data["formFields"]->$name->props->label);
+        }
+      }
+    }
+
+    //Response
+    return $data;
   }
 }
