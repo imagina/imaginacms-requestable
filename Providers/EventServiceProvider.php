107a8ea97deb4a6313aba1a209ad7e0d9326c828
William Verde <williamverde10@gmail.com>
2022-11-15 09:42:28 -0400

    Update: Event service provider

--- Providers/EventServiceProvider.php
+++ Providers/EventServiceProvider.php
@@ -5,24 +5,28 @@ namespace Modules\Requestable\Providers;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 //Events
+use Modules\Requestable\Events\RequestableWasCreated;
 use Modules\Requestable\Events\RequestableWasUpdated;
 
 //Handlers
 use Modules\Requestable\Events\Handlers\CheckStatusRequestable;
 use Modules\Iforms\Events\LeadWasCreated;
-use Modules\Requestable\Events\Handlers\CreateRequestableByLeadData;
+//use Modules\Requestable\Events\Handlers\CreateRequestableByLeadData;
 
 
 class EventServiceProvider extends ServiceProvider
 {
     protected $listen = [
-        /*
-        RequestableWasUpdated::class => [
-            CheckStatusRequestable::class
+
+        RequestableWasCreated::class => [
+            //heckStatusRequestable::class // Dejar comentador aun en prueba
         ],
-        */
-      LeadWasCreated::class => [
-        CreateRequestableByLeadData::class
-      ],
+        RequestableWasUpdated::class => [
+            //CheckStatusRequestable::class // Dejar comentador aun en prueba
+        ],
+        LeadWasCreated::class => [
+            CreateRequestableByLeadData::class
+        ],
+        
     ];
 }
