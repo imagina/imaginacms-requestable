27c36435476c8cd107839cd9b63c5b51b4243bcb
Juan Escalante <jcec007@gmail.com>
2023-01-27 16:56:10 -0500

    +Update: changed the form fields in the category rules config and replacing the filter categoryId in the form fields when the front send it in the request

--- Config/config.php
+++ Config/config.php
@@ -2,7 +2,7 @@
 
 return [
   'name' => 'Requestable',
-
+  
   /*
   |--------------------------------------------------------------------------
   | Define all the exportable available
@@ -15,7 +15,7 @@ return [
       'exportName' => "RequestablesExport"
     ]
   ],
-
+  
   /*
   |--------------------------------------------------------------------------
   | Same params Readme - Requestable - Only execute by CreateForm Seeder
@@ -23,172 +23,207 @@ return [
   */
   "requestable-leads" => [
     
-      //Required: this is the identificator of the request, must be unique with respect to other requestable types 
-      "type" => "leads",
-     
-      // Title can be trantaled or not, the language take the config app.locale 
-      "title" => "requestable::categories.leads.title",
-           
-      // Optional: This columns has true by default
-      "internal" => false,
-        
-      
-      //Optional: if the useDefaultStatuses is true, statuses is ignored 
-      "statuses" => [
-        1 => [
-            "id" => 1,
-            "title" => "requestable::statuses.leads.new", // Title can be trantaled or not, the language take the config app.locale 
-            "default" => true
-        ],
-        2 => [
-            "id" => 2,
-            "title" => "requestable::statuses.leads.contacted"
-        ],
-        3 => [
-            "id" => 3,
-            "title" => "requestable::statuses.leads.commercial proposal"
-        ],
-        4 => [
-            "id" => 4,
-            "title" => "requestable::statuses.leads.in progress"
-        ],
-        5 => [
-            "id" => 5,
-            "title" => "requestable::statuses.leads.successful",
-            "final" => true
-        ],
-        6 => [
-            "id" => 6,
-            "title" => "requestable::statuses.leads.lost",
-            "final" => true
-        ],
-      ]
-      
+    //Required: this is the identificator of the request, must be unique with respect to other requestable types
+    "type" => "leads",
+    
+    // Title can be trantaled or not, the language take the config app.locale
+    "title" => "requestable::categories.leads.title",
+    
+    // Optional: This columns has true by default
+    "internal" => false,
+    
+    
+    //Optional: if the useDefaultStatuses is true, statuses is ignored
+    "statuses" => [
+      1 => [
+        "id" => 1,
+        "title" => "requestable::statuses.leads.new", // Title can be trantaled or not, the language take the config app.locale
+        "default" => true
+      ],
+      2 => [
+        "id" => 2,
+        "title" => "requestable::statuses.leads.contacted"
+      ],
+      3 => [
+        "id" => 3,
+        "title" => "requestable::statuses.leads.commercial proposal"
+      ],
+      4 => [
+        "id" => 4,
+        "title" => "requestable::statuses.leads.in progress"
+      ],
+      5 => [
+        "id" => 5,
+        "title" => "requestable::statuses.leads.successful",
+        "final" => true
+      ],
+      6 => [
+        "id" => 6,
+        "title" => "requestable::statuses.leads.lost",
+        "final" => true
+      ],
+    ]
+  
   ],
-
+  
   /*
   |--------------------------------------------------------------------------
   | Categories Rule to Seeder
   |--------------------------------------------------------------------------
   */
   "categories-rules" => [
-        //Parent
-        0 => [
-            'systemName' => 'client-communications',
-            'en' => ['title' => 'Client Communications'],
-            'es' => ['title' => 'Comunicacion con el Cliente'],
-       ],
-       //Child Category - client-communications
-       1 => [
-            'systemName' => 'send-email',
-            'parentSystemName' => 'client-communications', //from parent
-            'en' => ['title' => 'Send Email'],
-            'es' => ['title' => 'Enviar email'],
-            'options' => ['filterFormFieldType'=>'email'],//type field iform
-            'formFields' => [
-                'from' => [
-                    'value' => null,
-                    'name' => 'from',
-                    'type' => 'select',
-                    'loadOptions' => [
-                      'apiRoute' => 'apiRoutes.quser.users',
-                      'select' => ['label' => 'email', 'value' => 'email', 'id' => 'email'],
-                    ],
-                    'props' => [
-                      'label' => 'requestable::common.formFields.from',
-                      'multiple' => false,
-                      'clearable' => true,
-                    ],
-                ],
-                'subject' => [
-                    'value' => null,
-                    'name' => 'subject',
-                    'type' => 'input',
-                    'isTranslatable' => true,
-                    'props' => [
-                        'label' => 'requestable::common.formFields.subject'
-                    ]
-                ],
-                'message' => [
-                    'value' => null,
-                    'name' => 'message',
-                    'type' => 'input',
-                    'isTranslatable' => true,
-                    'props' => [
-                        'label' => 'requestable::common.formFields.message',
-                        'type' => 'textarea',
-                        'rows' => 3,
-                    ]
-                ],
-            ]
-       ],
-       //Child Category - client-communications
-        2 => [
-            'systemName' => 'send-sms',
-            'parentSystemName' => 'client-communications', //from parent
-            'en' => ['title' => 'Send SMS'],
-            'es' => ['title' => 'Enviar sms'],
-            'status' => 0,
-            'options' => ['filterFormFieldType'=>'phone'],//type field iform
-            'formFields' => [
-                'message' => [
-                    'value' => null,
-                    'name' => 'message',
-                    'type' => 'input',
-                    'isTranslatable' => true,
-                    'props' => [
-                        'label' => 'requestable::common.formFields.message',
-                        'type' => 'textarea',
-                        'rows' => 3,
-                    ]
-                ]
-            ]
+    //Parent
+    0 => [
+      'systemName' => 'client-communications',
+      'en' => ['title' => 'Client Communications'],
+      'es' => ['title' => 'Comunicacion con el Cliente'],
+    ],
+    //Child Category - client-communications
+    1 => [
+      'systemName' => 'send-email',
+      'parentSystemName' => 'client-communications', //from parent
+      'en' => ['title' => 'Send Email'],
+      'es' => ['title' => 'Enviar email'],
+      'options' => ['filterFormFieldType' => 'email'],//type field iform
+      'formFields' => [
+        'from' => [
+          'value' => null,
+          'name' => 'from',
+          'type' => 'select',
+          'loadOptions' => [
+            'apiRoute' => 'apiRoutes.quser.users',
+            'select' => ['label' => 'email', 'value' => 'email', 'id' => 'email']
+          ],
+          'props' => [
+            'label' => 'requestable::common.formFields.from',
+            'multiple' => false,
+            'clearable' => true,
+          ],
         ],
-        //Child Category - client-communications
-        3 => [
-            'systemName' => 'send-telegram',
-            'parentSystemName' => 'client-communications', //from parent
-            'en' => ['title' => 'Send Telegram'],
-            'es' => ['title' => 'Enviar Telegram'],
-            'status' => 0,
-            'options' => ['filterFormFieldType'=>'phone'],//type field iform
-            'formFields' => [
-                'message' => [
-                    'value' => null,
-                    'name' => 'message',
-                    'type' => 'input',
-                    'isTranslatable' => true,
-                    'props' => [
-                        'label' => 'requestable::common.formFields.message',
-                        'type' => 'textarea',
-                        'rows' => 3,
-                    ]
-                ]
+        'subject' => [
+          'value' => null,
+          'name' => 'subject',
+          'type' => 'expression',
+          'isTranslatable' => true,
+          'loadOptions' => [
+            'apiRoute' => 'apiRoutes.qrequestable.categoriesFormFields',
+            'select' => ['label' => 'label', 'id' => 'value'],
+            'parametersUrl' => [
+              'categoryId' => 1
             ]
+          ],
+          'props' => [
+            'label' => 'requestable::common.formFields.subject'
+          ]
         ],
-        //Child Category - client-communications
-        4 => [
-            'systemName' => 'send-whatsapp',
-            'parentSystemName' => 'client-communications', //from parent
-            'en' => ['title' => 'Send Whatsapp'],
-            'es' => ['title' => 'Enviar Whatsapp'],
-            'options' => ['filterFormFieldType'=>'phone'],//type field iform
-            'formFields' => [
-                'message' => [
-                    'value' => null,
-                    'name' => 'message',
-                    'type' => 'input',
-                    'isTranslatable' => true,
-                    'props' => [
-                        'label' => 'requestable::common.formFields.message',
-                        'type' => 'textarea',
-                        'rows' => 3,
-                    ]
-                ]
+        'message' => [
+          'value' => null,
+          'name' => 'message',
+          'type' => 'expression',
+          'isTranslatable' => true,
+          'loadOptions' => [
+            'apiRoute' => 'apiRoutes.qrequestable.categoriesFormFields',
+            'select' => ['label' => 'label', 'id' => 'value'],
+            'parametersUrl' => [
+              'categoryId' => 1
             ]
+          ],
+          'props' => [
+            'label' => 'requestable::common.formFields.message',
+            'type' => 'textarea',
+            'rows' => 3,
+          ]
         ],
-
-  ]
+      ]
+    ],
+    //Child Category - client-communications
+    2 => [
+      'systemName' => 'send-sms',
+      'parentSystemName' => 'client-communications', //from parent
+      'en' => ['title' => 'Send SMS'],
+      'es' => ['title' => 'Enviar sms'],
+      'status' => 0,
+      'options' => ['filterFormFieldType' => 'phone'],//type field iform
+      'formFields' => [
+        'message' => [
+          'value' => null,
+          'name' => 'message',
+          'type' => 'expression',
+          'isTranslatable' => true,
+          'loadOptions' => [
+            'apiRoute' => 'apiRoutes.qrequestable.categoriesFormFields',
+            'select' => ['label' => 'label', 'id' => 'value'],
+            'parametersUrl' => [
+              'categoryId' => 1
+            ]
+          ],
+          'props' => [
+            'label' => 'requestable::common.formFields.message',
+            'type' => 'textarea',
+            'rows' => 3,
+          ]
+        ]
+      ]
+    ],
+    //Child Category - client-communications
+    3 => [
+      'systemName' => 'send-telegram',
+      'parentSystemName' => 'client-communications', //from parent
+      'en' => ['title' => 'Send Telegram'],
+      'es' => ['title' => 'Enviar Telegram'],
+      'status' => 0,
+      'options' => ['filterFormFieldType' => 'phone'],//type field iform
+      'formFields' => [
+        'message' => [
+          'value' => null,
+          'name' => 'message',
+          'type' => 'expression',
+          'loadOptions' => [
+            'apiRoute' => 'apiRoutes.qrequestable.categoriesFormFields',
+            'select' => ['label' => 'label', 'id' => 'value'],
+            'parametersUrl' => [
+              'categoryId' => 1
+            ]
+          ],
+          'isTranslatable' => true,
+          'props' => [
+            'label' => 'requestable::common.formFields.message',
+            'type' => 'textarea',
+            'rows' => 3,
+          ]
+        ]
+      ]
+    ],
+    //Child Category - client-communications
+    4 => [
+      'systemName' => 'send-whatsapp',
+      'parentSystemName' => 'client-communications', //from parent
+      'en' => ['title' => 'Send Whatsapp'],
+      'es' => ['title' => 'Enviar Whatsapp'],
+      'options' => ['filterFormFieldType' => 'phone'],//type field iform
+      'formFields' => [
+        'message' => [
+          'value' => null,
+          'name' => 'message',
+          'type' => 'expression',
+          'loadOptions' => [
+            'apiRoute' => 'apiRoutes.qrequestable.categoriesFormFields',
+            'select' => ['label' => 'label', 'id' => 'value'],
+            'parametersUrl' => [
+              'categoryId' => 1
+            ]
+          ],
+          'isTranslatable' => true,
+          'props' => [
+            'label' => 'requestable::common.formFields.message',
+            'type' => 'textarea',
+            'rows' => 3,
+          ]
+        ]
+      ]
+    ],
   
+  ]
+
 
 ];
--- Transformers/CategoryRuleTransformer.php
+++ Transformers/CategoryRuleTransformer.php
@@ -17,10 +17,21 @@ class CategoryRuleTransformer extends CrudResource
       'statusName' => $this->statusName,
       'formFields' => $this->form_fields,
     ];
-
+    
+    //getting filter from the request
+    $filter = (is_string($request->input('filter')) ? json_decode($request->input('filter')) : json_decode(json_encode($request->input('filter'))));
+    
+    
     //Translate the labels of the formfields
     if (!is_null($this->form_fields)) {
       foreach ($this->form_fields as $name => $field){
+       
+       //if the field is expression and the filter categoryId is coming in the request it'll be replaced in the field
+        if(isset($field->type) && $field->type == "expression" && isset($filter->categoryId) && !empty($filter->categoryId)){
+          $data["formFields"]->$name->loadOptions->parametersUrl->categoryId = $filter->categoryId;
+        }
+        
+        
         if(isset($data["formFields"]->$name->props->label)){
           $data["formFields"]->$name->props->label = trans($data["formFields"]->$name->props->label);
         }
