4914a3ae451c32e23e60928542f7ccfed2042e3d
Cristian Marin <marin101998@gmail.com>
2023-04-26 12:16:57 -0500

    +add | modules seeds by jobs

--- Database/Seeders/RequestableDatabaseSeeder.php
+++ Database/Seeders/RequestableDatabaseSeeder.php
@@ -4,21 +4,23 @@ namespace Modules\Requestable\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use Modules\Isite\Jobs\ProcessSeeds;
 
 class RequestableDatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        Model::unguard();
-
-        $this->call(RequestableTableSeeder::class);
-        $this->call(CreateFormTableSeeder::class);
-        //Dejar comentado xq aun sta en prueba
-        //$this->call(CreateCategoriesRulesTableSeeder::class);
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    Model::unguard();
+    ProcessSeeds::dispatch([
+      "baseClass" => "\Modules\Requestable\Database\Seeders",
+      "seeds" => ["RequestableTableSeeder","CreateFormTableSeeder"]
+    ]);
+    //Dejar comentado xq aun sta en prueba
+    //$this->call(CreateCategoriesRulesTableSeeder::class);
+  }
 }
