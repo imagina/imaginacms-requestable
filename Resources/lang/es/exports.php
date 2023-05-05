e1f31bbfc0d1a25b85f9c0f25f00b9fe191961fd
William Verde <williamverde10@gmail.com>
2023-05-05 17:56:41 -0400

    Added: new translations to new reports

---
+++ Resources/lang/es/exports.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+  
+  'exportFields' => [
+    'report type' => 'Tipo de Reporte',
+    'type' => [
+      'detailed' => 'Detallado',
+      'statuses' => 'Historial de Estados'
+    ]
+  ],
+
+  
+
+];
